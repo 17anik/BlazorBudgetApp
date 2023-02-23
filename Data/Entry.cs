15468db3744ce6d1b76dfc79858ea6c1a480a1e5
17ani <17ani@MSI>
2023-02-23 17:28:17 +0530

    Add project files.

---
+++ Data/Entry.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorBudgetApp.Data
+{
+    public class Entry
+    {
+        public decimal Amount { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ Data/budgetBookService.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorBudgetApp.Data
+{
+    public class budgetBookService
+    {
+        private static List<Entry> mockDb = new List<Entry>()
+        {
+            new Entry(){Description="Howgarts Legacy",Amount=3499m}
+        };
+
+        public async Task<bool> AddEntry(Entry newEntry)
+        {
+            mockDb.Add(newEntry);
+            return true;
+        }
+
+        public async Task<List<Entry>> GetAllEntries()
+        {
+            return mockDb;
+        }
+    }
+}
